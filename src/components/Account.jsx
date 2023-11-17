38f80c69a370e67b5a8cc69be29bca2d95862d25
jack <jackg2667@protonmail.com>
2023-11-17 17:02:40 +0000

    adding some testing h1s on the account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -19,7 +19,16 @@ const Account = () => {
     <div className="max-w-[600px] mx-auto my-16 p-4">
       <h1 className="text-2xl font-bold py-4">Accounts</h1>
       <p>User Email: {user && user.email}</p>
-      <h1>this is a texs</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+
       <button onClick={handleLogout} className="border px-6 py-2 my-4">
         Logout
       </button>
