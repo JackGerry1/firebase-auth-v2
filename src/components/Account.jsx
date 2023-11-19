c32a2927a363467aa87478e75d4eff8520172b0a
jack <jackg2667@protonmail.com>
2023-11-19 19:04:26 +0000

    removing github actions

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -20,6 +20,12 @@ const Account = () => {
       <h1 className="text-2xl font-bold py-4">Accounts</h1>
       <p>User Email: {user && user.email}</p>
       <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
+      <h1 className="text-2xl font-bold py-4">this is a test</h1>
 
 
       <button onClick={handleLogout} className="border px-6 py-2 my-4">
